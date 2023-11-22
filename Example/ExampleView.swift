c04c774e539942b3dce807a3ef3eda5e2badaf6a
kashif Mehmood <kashismails@gmail.com>
2023-11-22 22:48:23 +0500

    Added example and setup cocoapod library

---
+++ Example/ExampleView.swift
@@ -0,0 +1,19 @@
+//
+//  ExampleView.swift
+//  MaterialTextFieldIOS_Tests
+//
+//  Created by Kashif Mehmood on 22/11/2023.
+//  Copyright © 2023 CocoaPods. All rights reserved.
+//
+
+import SwiftUI
+
+struct ExampleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExampleView()
+}
